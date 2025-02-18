c9377193ef6ea5225563b393f5e2e8df02601b4b
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-02-18 08:15:06 +0000

    initial skeleton

---
+++ src/test/java/uk/gov/companieshouse/chs/notification/sender/api/ChsNotificationSenderApiApplicationTests.java
@@ -0,0 +1,14 @@
+package uk.gov.companieshouse.chs.notification.sender.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ChsNotificationSenderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
