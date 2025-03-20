ebeedfa12b6df1d577b955411d86eed291589138
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-03-20 09:57:57 +0000

    Initial push

--- src/main/java/uk/gov/companieshouse/chs/notification/kafka/consumer/apiintegration/ApiIntegrationService.java
+++ src/main/java/uk/gov/companieshouse/chs/notification/kafka/consumer/apiintegration/ApiIntegrationService.java
@@ -1,48 +1,62 @@
-//package uk.gov.companieshouse.chs.notification.kafka.consumer.apiintegration;
-//
-//import org.springframework.stereotype.Service;
-//import uk.gov.companieshouse.api.handler.payment.PrivatePaymentResourceHandler;
-//
-//import java.net.URI;
-//import java.net.http.HttpClient;
-//import java.net.http.HttpRequest;
-//import java.net.http.HttpResponse;
-//import java.net.http.HttpRequest;
-//
-//@Service
-//public class ApiIntegrationService {
-//
-////ResourceHandler would be the best option to consider look up:
-////PrivatePaymentResourceHandler
-////below using java generic http builder
-//
-//    public ApiIntegrationService(ApiIntegrationController apiIntegrationController) {
-//        this.apiIntegrationController = apiIntegrationController;
-//    }
-//
-//    ApiIntegrationController apiIntegrationController;
-//
-//    public void sendEmail(GovUKEmailRequest govUKEmailRequest, id) {
-//        String url = apiIntegrationController.getNotificationApiUrl() + "/email/" + id;
-//        HttpClient client = HttpClient.newHttpClient();
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(new URI(url))
-//                .header("Content-Type", "application/json")
-//                .POST(HttpRequest.BodyPublishers.ofString("{\"key\":\"value\"}"))
-//                .build();
-//
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//    }
-//
-//    public void sendLetter(GovUkLetterRequest govUkLetterRequest) {
-//        String url = apiIntegrationController.getNotificationApiUrl() + "/letter" + id;
-//        HttpClient client = HttpClient.newHttpClient();
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(new URI(url))
-//                .header("Content-Type", "application/json")
-//                .POST(HttpRequest.BodyPublishers.ofString("{\"key\":\"value\"}"))
-//                .build();
-//
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//    }
-//}
+package uk.gov.companieshouse.chs.notification.kafka.consumer.apiintegration;
+
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.api.chs_notification_sender.model.GovUkEmailDetailsRequest;
+import uk.gov.companieshouse.api.chs_notification_sender.model.GovUkLetterDetailsRequest;
+import uk.gov.companieshouse.api.handler.payment.PrivatePaymentResourceHandler;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class ApiIntegrationService {
+
+//ResourceHandler would be the best option to consider look up:
+//            PrivatePaymentResourceHandler?
+
+    public ApiIntegrationService(ApiIntegrationController apiIntegrationController) {
+        this.apiIntegrationController = apiIntegrationController;
+    }
+
+    ApiIntegrationController apiIntegrationController;
+
+
+    public void sendEmail(GovUkEmailDetailsRequest govUkEmailDetailsRequest) {
+        String url = apiIntegrationController.getNotificationApiUrl() + "/email/";
+        HttpClient client = HttpClient.newHttpClient();
+
+        try {
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString("{\"key\":\"value\"}"))
+                    .build();
+
+            HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendLetter(GovUkLetterDetailsRequest govUkLetterRequest) {
+        String url = apiIntegrationController.getNotificationApiUrl() + "/letter/";
+        HttpClient client = HttpClient.newHttpClient();
+
+        try {
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString("{\"key\":\"value\"}"))
+                    .build();
+
+            HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/uk/gov/companieshouse/chs/notification/kafka/consumer/apiIntegration/ApiIntegrationServiceTest.java
@@ -0,0 +1,9 @@
+package uk.gov.companieshouse.chs.notification.kafka.consumer.apiIntegration;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@Tag("unit-test")
+public class ApiIntegrationServiceTest {}
